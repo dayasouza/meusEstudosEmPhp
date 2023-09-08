c78885a45849a6d04349e585d33c36b61e7ca7a9
dayasouza <dayanalvesouza@gmail.com>
2023-09-08 20:05:35 -0300

    Adicionado exercicio 04 do curso de php, onde foi aprendido sobre tipos de dados

---
+++ buddyBr_aula04_tipos_de_dados/pessoa.php
@@ -0,0 +1,26 @@
+<?php
+class Pessoa {
+    public $nome;
+    private $sobrenome;
+    private $ano_de_nascimento;
+    private $data;
+    private $idade;
+
+    function __construct($name, $surname, $year)
+    {
+        $this -> nome = $name;
+        $this -> sobrenome = $surname;
+        $this -> ano_de_nascimento = $year;
+        $this -> data = date('Y', time());
+        $this -> calc_idade();
+        
+    }
+
+    public function display_info($var){
+        echo $var;
+    }
+
+    private function calc_idade(){
+        $this -> idade = $this -> data - $this -> ano_de_nascimento;
+    }
+}
