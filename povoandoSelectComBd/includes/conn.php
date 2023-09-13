3a7d649d56c621624c638211116c05130f190f5a
dayasouza <dayanalvesouza@gmail.com>
2023-09-12 21:42:17 -0300

    Alterado função de conexão com o banco de dados de PDO para mysqli

--- povoandoSelectComBd/includes/conn.php
+++ povoandoSelectComBd/includes/conn.php
@@ -1,15 +1,12 @@
 <?php
-
 $host = 'localhost';
 $user = 'root';
 $pass = '';
 $db = 'dbteste';
 
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
-    $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $conn -> setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-} catch (Exception $e) {
-    echo $e -> getMessage();
-    exit;
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
 }
+?>
